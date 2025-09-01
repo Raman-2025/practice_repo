72483b52315228520eb041207ec166bd928e0a9f
Raman-2025 <rmnkr8@gmail.com>
2025-09-01 13:45:59 +0530

    add way 2

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ print ("this is my practice repo so in main branch")
 print ("create new repo in vs ")
 print("STEPS<br>" \
 " 1 : i create this repo first in my local than i do init command " \
-"2" : i create new branch front then change in this branch now i add this through github\
+"2" : i create new branch front then change in this branch now i add this through github with way 2 PR\
 "3" \
 "4" \
 "5" \
