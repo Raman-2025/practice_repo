b23419530dc7bc8e7cd8d0d6894d46d1dbc84ce3
Raman-2025 <rmnkr8@gmail.com>
2025-09-01 13:35:45 +0530

    add new branch front changes

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ print ("this is my practice repo so in main branch")
 print ("create new repo in vs ")
 print("STEPS<br>" \
 " 1 : i create this repo first in my local than i do init command " \
-"2" \
+"2" : i create new branch front then change in this branch now i add this through github\
 "3" \
 "4" \
 "5" \
