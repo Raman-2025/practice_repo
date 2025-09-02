8b62c84de9d088a200577a11e7395e5073809746
Raman-2025 <rmnkr8@gmail.com>
2025-09-02 18:25:56 +0530

    Update index.py

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ print("STEPS<br>" \
 " 1 : i create this repo first in my local than i do init command " \
 "2:  and back" \
      <br>"2a : it main branch" \
-"3" \
+"3 change " \
 "4" \
 "5" \
 "6" \
