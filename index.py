13917f8b93ce46f5edf1fb6cb7ed83619d23e7ca
Raman-2025 <rmnkr8@gmail.com>
2025-09-02 18:19:02 +0530

    add back squash

--- index.py
+++ index.py
@@ -2,7 +2,8 @@ print ("this is my practice repo so in main branch")
 print ("create new repo in vs ")
 print("STEPS<br>" \
 " 1 : i create this repo first in my local than i do init command " \
-"2: it main branch" \
+"2:  and back" \
+     <br>"2a : it main branch" \
 "3" \
 "4" \
 "5" \
