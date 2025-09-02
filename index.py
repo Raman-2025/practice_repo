e4834d53e8069adbe768bd2fb1e810987f4f26bd
Raman-2025 <rmnkr8@gmail.com>
2025-09-02 18:38:51 +0530

    h

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ print("STEPS<br>" \
 " 1 : i create this repo first in my local than i do init command " \
 "2:  and back" \
      "2a: it main branch" \
-"3 =a=b" \
+"3 =a=b=c" \
 "4" \
 "5" \
 "6" \
