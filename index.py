46d5caace0a079aac5125645bf4ee036b0d5c74d
Raman-2025 <rmnkr8@gmail.com>
2025-09-02 11:53:42 +0530

    Initial commit with css folder

---
+++ index.py
@@ -0,0 +1,11 @@
+print ("this is my practice repo so in main branch")
+print ("create new repo in vs ")
+print("STEPS<br>" \
+" 1 : i create this repo first in my local than i do init command " \
+"2" \
+"3" \
+"4" \
+"5" \
+"6" \
+""
+)
